a1d3098a1681d79b33c11ffdbf19c23f43789f86
steclaudinodaffara <ste.daff@gmail.com>
2014-01-12 03:24:42 -0800

    got existing clients to adopt animals

--- main.rb
+++ main.rb
@@ -3,8 +3,6 @@ require './animal'
 require './client'
 require 'pry'
 
-shelter = Shelter.new("HappiTails")
-
 def menu (message)
 	puts `clear`
 	puts "*** Managing your Animal Shelter ***\n\n"
@@ -24,69 +22,97 @@ def menu (message)
 
 end
 
-message =""
-choice = menu(message)
+def choices (choice)
+	shelter = Shelter.new("HappiTails")
 
-while choice != 'q'
-	message = ""
-	case choice
+	while choice != 'q'
+		message = ""
 
-	when '1'
-		message = "The animals in the shelter are: "
-		shelter.animals.each { |pet| message += pet.name + " "  }
-	when '2'
-		message = "The current clients of this shelter are: n/"
-		shelter.clients.each { |client| message += client.name + " "  }
+		case choice
 
-	when '3'
-		print "name: "; name = gets.chomp
-		print "age: "; age = gets.chomp
-		print "gender: "; gender = gets.chomp
-		print "species: "; species = gets.chomp
-		print "toys? If any... : "; toys = gets.chomp.split(", ")
-		shelter.animals << Animal.new( name, age, gender, species, toys)
-		message += "Animal #{name} has been added to #{shelter.name} shelter"
+		when '1'
+			message = "The animals in the shelter are: \n"
+			shelter.animals.each { |pet| 
+				message += "#{pet.name}: #{pet.gender}: #{pet.species}:
+				 #{pet.age} years \n " }
+		when '2'
+			message = "The current clients of this shelter are: \n"
+			shelter.clients.each { |client| message += client.name + "\n "  }
 
-	when '4'
-		print "name: "; name = gets.chomp
-		print "age: "; age = gets.chomp
-		print "number of kids: "; num_kids = gets.chomp
-		print "number of pets: "; num_pets = gets.chomp
-		shelter.clients << Client.new( name, age, num_kids, num_pets)
-		message += "Client #{name} has been added to #{shelter.name} shelter's database"
+		when '3'
+			print "name: "; name = gets.chomp
+			print "age: "; age = gets.chomp
+			print "gender: "; gender = gets.chomp
+			print "species: "; species = gets.chomp
+			print "toys? If any... : "; toys = gets.chomp.split(", ")
+			shelter.animals << Animal.new( name, age, gender, species, toys)
+			message += "Animal #{name} has been added to #{shelter.name} shelter"
 
-	when '5'
-		print "We have the following pets up for adoptions: "
-		shelter.animals.each { |pet| puts pet.name + " "  }
-		print "Which one would you like do adopt? > "
-		clients_choice = gets.chomp
-		clients_pet = shelter.animals.select { |pet| 
-					pet.name == clients_choice }.first
+		when '4'
+			print "name: "; name = gets.chomp
+			print "age: "; age = gets.chomp
+			print "number of kids: "; num_kids = gets.chomp
+			print "number of pets: "; num_pets = gets.chomp
+			shelter.clients << Client.new( name, age, num_kids, num_pets)
+			message += "Client #{name} has been added to #{shelter.name} shelter's database"
 
-		print "Are you already a client? [yes] [no] > "
-		is_client = gets.chomp
-			case is_client
-			when "yes"
-				print "What's your name? > "
-				client_name = gets.chomp
-				current_client = shelter.clients.select { |person| 
-					person.name == client_name }.first
-				current_client.pets << clients_pet
-				
-			when "no"
-				choice = 4
-				# selected the added client to add the animal to
-			end
+		when '5'
+			print "We have the following pets up for adoptions: \n "
+			shelter.animals.each { |pet| puts pet.name + "\n" }
+			print "Which one would you like do adopt? > "
+			clients_choice = gets.chomp
+			clients_pet = shelter.animals.select { |pet| 
+						pet.name == clients_choice }.first
 
-		shelter.animals.select { |pet| pet.name == clients_choice }.first
+			# catch(:redo) do
+				print "Are you already a client? [yes] [no] >  "
+				is_client = gets.chomp
+				case is_client
+				when "yes"
+					print "What's your name? > "
+					client_name = gets.chomp 
+					current_client = shelter.clients.select { |person| 
+						person.name == client_name }.first
+					current_client.pets << clients_pet
+				message += "#{clients_choice} was adopted by #{client_name}!"
+				when "no"
+					message += 'select [4] to add the client'	
+					choices('4') #why doesn't this work!! NOW THE CODE simply starts over
+					# redo 
+					# how to run what comes after this method call
+				 #    current_client = shelter.clients.select { |person| 
+					# 	person.name == name }.first
+					# current_client.pets << clients_pet
 
+					
+				end
 
-	when '6'
+		when '6'
+			print "Are you already a client? [yes] [no] >  "
+				is_client = print gets.chomp
+				case is_client
+				when "yes"
+					print "What's your name? > "
+					client_name = gets.chomp 
+					current_client = shelter.clients.select { |person| 
+						person.name == client_name }.first
+					current_client.pets << clients_pet
+				when "no"
+					message += 'select [4] to add the client'	
+					choices('4') #why doesn't this work!! NOW THE CODE simply starts over
+				end
 
-	else 
-		message += "please select one of the options..."
+		else 
+			message += "please select one of the options..."
+
+		end
+		choice = menu(message)
 
 	end
-	choice = menu(message)
-
 end
+
+message = ""
+choice = menu(message)
+choices(choice)
+
+
