c185a008cab3fca488750633a8d25060e5bb83af
steclaudinodaffara <ste.daff@gmail.com>
2014-01-11 20:28:41 -0800

    print clients and animals names out nicely

--- main.rb
+++ main.rb
@@ -16,7 +16,7 @@ def menu (message)
 	puts '4 : add a client'
 	puts "5 : facilitate client's pet adoption"
 	puts "6 : faciliate client's pet put up for adoption"
-	puts 'q : Quit\n\n'
+	puts 'q : Quit'
 	puts '-->'
 	puts 
 	gets.chomp
@@ -31,32 +31,32 @@ while choice != 'q'
 	case choice
 
 	when '1'
-		message = "The available animals in the shelter are: n/"
-		message += shelter.animals() + " " #check to see if this will print out nicely 
-
+		message = "The animals in the shelter are: "
+		shelter.animals.each { |pet| message += pet.name + " "  }
 	when '2'
 		message = "The current clients of this shelter are: n/"
-		message += shelter.clients() + " "
+		shelter.clients.each { |client| message += client.name + " "  }
 
 	when '3'
 		print "name: "; name = gets.chomp
 		print "age: "; age = gets.chomp
-		print "gender: " gender = gets.chomp
-		print "species: " species = gets.chomp
-		print "toys? If any... : " toys = gets.chomp.split (", ")
-		shelter.animals << new.Animal( name, age, gender, species, toys)
-		message += "Animal #{name} has been added to #{shelter}"
+		print "gender: "; gender = gets.chomp
+		print "species: "; species = gets.chomp
+		print "toys? If any... : "; toys = gets.chomp.split(", ")
+		shelter.animals << Animal.new( name, age, gender, species, toys)
+		message += "Animal #{name} has been added to #{shelter.name} shelter"
 
 	when '4'
 		print "name: "; name = gets.chomp
 		print "age: "; age = gets.chomp
-		print "number of kids: " num_kids = gets.chomp
-		print "number of pets: " num_pets = gets.chomp
-		shelter.clients << new.Client( name, age, num_kids, num_pets)
+		print "number of kids: "; num_kids = gets.chomp
+		print "number of pets: "; num_pets = gets.chomp
+		shelter.clients << Client.new( name, age, num_kids, num_pets)
 		message += "Animal #{name} has been added to #{shelter}"
 
 	when '5'
-		print ""
+		print "We have the following pets up for adoptions: "
+		puts shelter.animals.select.map { |pet| pet.name  }
 
 	when '6'
 
